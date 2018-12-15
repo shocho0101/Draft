7bf157e2c6861ce6cecdab93a8c7c6bcee606683
張翔 <chosho.0101@softbank.ne.jp>
2018-12-15 12:44:53 +0900

    Initial Commit

---
+++ Draft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Draft
+//
+//  Created by 張翔 on 2018/12/15.
+//  Copyright © 2018 張翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
